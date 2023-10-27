87a1952616b692b62c8c63ba2886b3d382acd047
Pranav Vashi <neobuddy89@gmail.com>
2023-10-28 02:43:46 +0530

    vendor: Build GameSpace
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: DenlNister <dcnnn950711@gmail.com>

--- config/rurise.mk
+++ config/rurise.mk
@@ -30,3 +30,7 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
 # Enable dex2oat64 to do dexopt
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
     dalvik.vm.dex2oat64.enabled=true
+
+# Extra packages
+PRODUCT_PACKAGES += \
+    GameSpace
